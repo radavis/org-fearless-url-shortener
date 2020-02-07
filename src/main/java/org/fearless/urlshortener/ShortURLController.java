6e61540a7f30116703274b788818e99ce3f29e8b
Richard Davis <rdavis.bacs@gmail.com>
2020-02-07 09:40:04 -0500

    Handle GET and POST for a short url

---
+++ src/main/java/org/fearless/urlshortener/ShortURLController.java
@@ -0,0 +1,41 @@
+package org.fearless.urlshortener;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import static org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder.on;
+
+@Controller
+public class ShortURLController {
+
+    private final ShortURLService shortURLService;
+
+    @Autowired
+    public ShortURLController(ShortURLService shortURLService) {
+        this.shortURLService = shortURLService;
+    }
+
+    @GetMapping("/{shortCode}")
+    public ResponseEntity redirectToURL(@PathVariable("shortCode") String shortCode) {
+        ShortURL shortURL = shortURLService.expandShortCode(shortCode);
+        return ResponseEntity.status(HttpStatus.FOUND)
+            .location(shortURL.getUri())
+            .build();
+    }
+
+    @PostMapping("/")
+    public ResponseEntity shortenURL(@RequestParam URI uri) {
+        ShortURL shortURL = shortURLService.shortenURL(uri);
+        URI shortenedURL = URI.create("http://localhost:8080/" + shortURL.getShortCode());
+        return ResponseEntity.created(shortenedURL).build();
+    }
+}
--- src/main/java/org/fearless/urlshortener/ShortURLService.java
+++ src/main/java/org/fearless/urlshortener/ShortURLService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
+@Service
 public class ShortURLService {
 
     private static final String SPACE = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_";
@@ -22,12 +23,12 @@ public class ShortURLService {
     }
 
     /**
-	 * Create a new {@link ShortURL} using the given URI and save it
-	 * in a datastore. The {@code shortCode} used to identify this
-	 * URI should be randomly selected.
-	 * @param uri the URI to shorten
-	 * @return the shortened URL information
-	 */
+     * Create a new {@link ShortURL} using the given URI and save it
+     * in a datastore. The {@code shortCode} used to identify this
+     * URI should be randomly selected.
+     * @param uri the URI to shorten
+     * @return the shortened URL information
+     */
     public ShortURL shortenURL(URI uri) {
         int n = Math.abs(new Random().nextInt());
         ShortURL result = new ShortURL(uri, encode(n));
@@ -36,13 +37,13 @@ public class ShortURLService {
     }
 
     /**
-	 * Return the {@link ShortURL} information for the given
-	 * {code shortCode}, if it exists in the datastore.
-	 * @param shortCode the shortened URL code to look for
-	 * @return the ShortURL information for the given shortCode
-	 * @throws ShortCodeNotFoundException if no ShortURL could be found
-	 * for the given shortCode
-	 */
+     * Return the {@link ShortURL} information for the given
+     * {code shortCode}, if it exists in the datastore.
+     * @param shortCode the shortened URL code to look for
+     * @return the ShortURL information for the given shortCode
+     * @throws ShortCodeNotFoundException if no ShortURL could be found
+     * for the given shortCode
+     */
     public ShortURL expandShortCode(String shortCode) {
         return shortURLRepository.findByShortCode(shortCode)
             .orElseThrow(() -> new ShortCodeNotFoundException(shortCode));
